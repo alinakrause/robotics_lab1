4380935bc5f7e322afd606be926d69f33690552d
alinakrause <alikrause@uni-osnabrueck.de>
2022-01-29 21:01:51 +0000

    second subscriber

--- scripts/publisher_node.py
+++ scripts/publisher_node.py
@@ -11,26 +11,34 @@ vel_cmd = Twist()
 
 
 def pose_callback(data):
-	vel_cmd.linear.x = pos_msg.kp * ((pos_msg.xd)-(data.kp * 100))
-	
+	global vel_cmd
+	vel_cmd.linear.x = data.x * 100
+	#rospy.loginfo(vel_cmd.linear.x)
 
-	
+def controller(data):
+	global pos_mag
+	pos_msg.kp = 1
+	pos_msg.xd = 8
+
 
 if __name__ == '__main__':
 	# initialize the node
 	rospy.init_node('pos_converter', anonymous = True)
 	# add a subscriber to it to read the position information
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+	rospy.Subscriber('/turtle1/control_params', Turtlecontrol,controller)
+	
 	# add a publisher with a new topic using the Shortpos message
-	pos_pub = rospy.Publisher('/turtle1/control_params', Turtlecontrol, queue_size = 10)
+	pos_pub = rospy.Publisher('/turtle1/turtlecontrol', Twist, queue_size = 10)
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
 	
 
 	while not rospy.is_shutdown():
-		
+		vel_cmd.linear.x = (pos_msg.kp*((pos_msg.xd)-(vel_cmd.linear.x)))
+		#rospy.loginfo(vel_cmd.linear.x)
 		# publish the message
-		pos_pub.publish(pos_msg)
+		pos_pub.publish(vel_cmd)
 		
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
