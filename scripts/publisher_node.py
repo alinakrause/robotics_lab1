5f2c37da74979a11347b3da421a32f092a481ab0
alinakrause <alikrause@uni-osnabrueck.de>
2022-01-29 19:47:48 +0000

    publisher

--- scripts/publisher_node.py
+++ scripts/publisher_node.py
@@ -3,12 +3,17 @@
 import rospy
 from turtlesim.msg import Pose
 from robotics_lab1.msg import Turtlecontrol
+from geometry_msgs.msg import Twist
 import math
 
 pos_msg = Turtlecontrol()
+vel_cmd = Twist()
+
 
 def pose_callback(data):
-	pos_msg.kp = data.kp * 100
+	vel_cmd.linear.x = pos_msg.kp * ((pos_msg.xd)-(data.kp * 100))
+	
+
 	
 
 if __name__ == '__main__':
@@ -20,10 +25,12 @@ if __name__ == '__main__':
 	pos_pub = rospy.Publisher('/turtle1/control_params', Turtlecontrol, queue_size = 10)
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
+	
 
 	while not rospy.is_shutdown():
-		pos_msg.xd = 500
+		
 		# publish the message
 		pos_pub.publish(pos_msg)
+		
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
