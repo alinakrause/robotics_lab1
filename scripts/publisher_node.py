de9672ce40a3239669640400f620b9cc39b75fcd
alinakrause <alikrause@uni-osnabrueck.de>
2022-02-01 02:03:20 +0000

    test

--- scripts/publisher_node.py
+++ scripts/publisher_node.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python3
 
 import rospy
+# we are going to read turtlesim/Pose messages this time
 from turtlesim.msg import Pose
+#importing the new message from our package
 from robotics_lab1.msg import Turtlecontrol
-from geometry_msgs.msg import Twist
+# for radians to degrees conversions
 import math
 
-pos_msg = Turtlecontrol()
-vel_cmd = Twist()
+ROTATION_SCALE = 180.0/math.pi
 
+pos_msg = Pose()
+test = Turtlecontrol()
 
 def pose_callback(data):
-	global vel_cmd
-	vel_cmd.linear.x = data.x * 100
-	#rospy.loginfo(vel_cmd.linear.x)
-
-def controller(data):
-	global pos_mag
-	pos_msg.kp = 1
-	pos_msg.xd = 8
-
-
+	global pos_msg
+	# convert angular position to degrees
+	#pos_msg.theta = data.theta * ROTATION_SCALE
+	# convert x and y to cm
+	pos_msg.x = data.x * 100
+	pos_msg.y = data.y * 100
+	
+def callback(data):
+	global test
+	#test.xd = data.xd + 1.0
+	test.xd = data.x * 100
+	rospy.loginfo(test.xd)
+	
+	
 if __name__ == '__main__':
 	# initialize the node
 	rospy.init_node('pos_converter', anonymous = True)
 	# add a subscriber to it to read the position information
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
-	rospy.Subscriber('/turtle1/control_params', Turtlecontrol,controller)
-	
+	rospy.Subscriber('/turtle1/param', Turtlecontrol, callback)
+	rospy.loginfo(test.xd)
 	# add a publisher with a new topic using the Shortpos message
-	pos_pub = rospy.Publisher('/turtle1/turtlecontrol', Twist, queue_size = 10)
+	pos_pub = rospy.Publisher('/turtle1/turtlecontrol', Pose, queue_size = 10)
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
-	
 
 	while not rospy.is_shutdown():
-		vel_cmd.linear.x = (pos_msg.kp*((pos_msg.xd)-(vel_cmd.linear.x)))
-		#rospy.loginfo(vel_cmd.linear.x)
 		# publish the message
-		pos_pub.publish(vel_cmd)
-		
+		pos_pub.publish(pos_msg)
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
